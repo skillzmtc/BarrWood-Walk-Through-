971c9aff0c733c68240addb5f3d2294dc261355f
skillzmtc <dave@barrwoodservices.com>
2026-07-27 19:51:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barrwood-walkthrough-v1';
+const CACHE_NAME = 'barrwood-walkthrough-v2';
 const FILES_TO_CACHE = [
   './index.html',
   './manifest.json',
@@ -29,8 +29,14 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
